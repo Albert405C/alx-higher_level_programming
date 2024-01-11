cc58cc39a19213cea78c1ffd60e1b3404c43eea4
Albert Mitto <94157652+Albert405C@users.noreply.github.com>
2024-01-11 09:13:06 +0300

    100-print_tebahpla.py

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
 for i in range(ord('z'), ord('A') - 1, -1):
-    print("{}".format(chr(i)), end="")
+    if i % 2 == 0:
+        print("{}".format(chr(i).lower()), end="")
+    else:
+        print("{}".format(chr(i).upper()), end="")
