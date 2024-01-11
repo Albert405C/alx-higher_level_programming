3100e4c98b88db1f6d7e232533db0f044a1afb75
Albert Mitto <94157652+Albert405C@users.noreply.github.com>
2024-01-11 09:18:54 +0300

    101-remove_char_at.py

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,9 +1,5 @@
-#!/usr/bin/python3
-def remove_char_at(input_str, n):
-    if n < 0 or n >= len(input_str):
-        # Check if the index is out of bounds
-        print("Index out of bounds.")
-        return input_str
-    result_str = input_str[:n] + input_str[n+1:]
-
-    return result_str
+!/usr/bin/python3
+def remove_char_at(str, n):
+    if n < 0:
+        return (str)
+    return (str[:n] + str[n+1:])
