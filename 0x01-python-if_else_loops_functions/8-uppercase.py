ba67bb0f618609018055baed58857a4a40c381b9
Albert Mitto <94157652+Albert405C@users.noreply.github.com>
2024-01-11 07:49:49 +0300

    8-uppercase.py

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def uppercase(input_str):
     for char in input_str:
-        if 97 <= ord(char) <= 122:  # ASCII values for lowercase letters
+        if 97 <= ord(char) <= 122: 
             char = chr(ord(char) - 32)
         print("{}".format(char), end="")
