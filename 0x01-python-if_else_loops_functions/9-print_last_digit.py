da8830dc99d2ef7c5db7d8c5a72040b3c853f299
Albert Mitto <94157652+Albert405C@users.noreply.github.com>
2024-01-11 08:21:42 +0300

    9-print_last_digit.py

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 i = 0
-last_digit = (abs) i % 10
+last_digit = abs(i) % 10
+
 def print_last_digit(number):
     for i in range(11):
-        if i is true:
-            return: last_digit
-        print("{}" .format(i), end="")
+        print("{}".format(i), end="")
+        last_digit = abs(i) % 10
