a963f2646b16daf43e1663ba0a019d2e38da5256
Albert Mitto <94157652+Albert405C@users.noreply.github.com>
2024-01-13 07:04:59 +0300

    2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,19 +1,15 @@
 #!/usr/bin/python3
+from sys import argv  
 if __name__ == "__main__":
-    # Get the number of arguments
-    num_args = len(argv) - 1  # Subtract 1 to exclude the script name itself
+    num_args = len(argv) - 1  
 
-    # Print the number of arguments
     if num_args == 0:
         print("Number of argument(s): 0.")
     elif num_args == 1:
         print("Number of argument(s): 1.")
     else:
         print("Number of argument(s): {}.".format(num_args))
-
-    # Print the list of arguments
     if num_args > 0:
         print()
         for i, arg in enumerate(argv[1:], start=1):
             print("{}: {}".format(i, arg))
-
