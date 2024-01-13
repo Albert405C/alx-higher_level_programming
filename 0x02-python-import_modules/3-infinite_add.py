8dce68033e5058e1d2eaf7286ec7567ea31a443e
Albert Mitto <94157652+Albert405C@users.noreply.github.com>
2024-01-13 07:10:53 +0300

    3-infinite_add.py

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     """print arguments"""
+    import sys
     arguments = argv[1:]
     result = sum(int(arg) for arg in arguments)
     print(result)
