406b83b0eb677ac6a3dea72e2b961c59e6823939
Albert Mitto <94157652+Albert405C@users.noreply.github.com>
2024-01-15 05:04:15 +0300

    3-print_reversed_list_integer.py

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,9 +1,3 @@
-#!/usr/bin/python3
-def replace_in_list(my_list, idx, element):
-    # Check if idx is negative or out of range
-    if idx < 0 or idx >= len(my_list):
-        return my_list
-
-    # Replace the element at the specified index
-    my_list[idx] = element
-    return my_list
+def print_reversed_list_integer(my_list=[]):
+    for num in reversed(my_list):
+        print("{:d}".format(num))
