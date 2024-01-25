bf75258eb5583fa832d82bfc942213d135b92652
Albert Mitto <94157652+Albert405C@users.noreply.github.com>
2024-01-25 04:19:10 +0300

    3-square.py

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
+"""Define a class Square."""
 
 
 class Square:
+    """Represent a square."""
+
     def __init__(self, size=0):
         """Initialize a new square.
         Args:
@@ -14,4 +17,5 @@ class Square:
         self.__size = size
 
     def area(self):
+        """Return the current area of the square."""
         return (self.__size * self.__size)
