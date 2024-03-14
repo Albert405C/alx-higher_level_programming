374c9135344b517388628b1d7f2208cd29748754
Albert Mitto <94157652+Albert405C@users.noreply.github.com>
2024-03-14 16:33:53 +0300

    10-top_score.sql

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,4 +1,4 @@
 -- a script to display all the records 
 -- the order should be in DESC order
 
-SELECT score , name FROM hbtn_0c_0.second_table ORDER BY score DESC;
+SELECT score , name FROM second_table ORDER BY score DESC;
