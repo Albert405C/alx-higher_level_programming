6bb67cda4e6bbb9eec7c8ceeea22fc2c27719a04
Albert Mitto <94157652+Albert405C@users.noreply.github.com>
2024-03-14 16:34:44 +0300

    11-best_score.sql

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,3 +1,3 @@
 -- list all records with a score of >=10
 
-SELECT score, name FROM hbtn_0c_0.second_table WHERE score >= 10 ORDER BY score DESC;
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
