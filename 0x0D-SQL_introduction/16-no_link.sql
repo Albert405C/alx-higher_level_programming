3ea2599dca8686f59d642e3e305db87812a0c6b5
Albert Mitto <94157652+Albert405C@users.noreply.github.com>
2024-03-14 16:40:45 +0300

    16-no_link.sql

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,3 +1,3 @@
 -- a script that lists all records of the table second_table of the database
 
-SELECT score, name FROM hbtn_0c_0.second_table  WHERE name IS NOT NULL ORDER BY score, name DESC;
+SELECT score, name FROM second_table  WHERE name IS NOT NULL ORDER BY score, name DESC;
