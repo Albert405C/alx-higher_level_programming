aa16d6440fb0e5175bc3e4d609ea1d10aef7e680
Albert Mitto <94157652+Albert405C@users.noreply.github.com>
2024-03-14 16:31:50 +0300

    9-full_creation.sql

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,6 +1,6 @@
 -- create a new table and insert values to them table
 
-CREATE TABLE IF NOT EXISTS hbtn_0c_0.second_table(
+CREATE TABLE IF NOT EXISTS second_table(
 	id INT,
 	name VARCHAR(256),
 	score INT
